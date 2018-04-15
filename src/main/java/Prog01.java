86a25b5825a021bd2c33fced9eeadd7d2a333e89
yc <XXXXXX.com>
2018-04-15 20:56:13 +0800

    第一题兔子

--- src/main/java/Prog01.java
+++ src/main/java/Prog01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  * Q
@@ -12,12 +14,33 @@ public class Prog01 {
 
 	public static void main(String[] args) {
 
- 
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("第几月：");
+		int month = scanner.nextInt();
+		
+		
+		int x = 1;//成熟的兔子数	
+		int i = 3;//天数
+		
+		int z = 0;//总数
+		
+		int[] a = new int[100];//每天兔子出生数
+		
+		
+		for (; i < month+1; i++) {
+
+			x=x+a[i-2];//今天所有成熟的兔子数 =昨天成熟兔子数   + 前天出生的兔子
+			a[i] = x;//今天出生兔子数（今天出生兔子数   =    今天成熟兔子数
+			
+
+		}
+		z=x+a[i-1]+a[i-2];	//所有兔子数   =    今天所有成熟的兔子数+昨天出生兔子数+前天出生小兔子数
+		System.out.println(z);
+
+	
 
 	}
  
-	private static long fun(int n) {
-		return 0;
-	}
 
 }
