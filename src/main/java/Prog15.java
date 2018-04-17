6ee11c20fc464318c82eeb9dc4a49a4afe9e47e0
yc <XXXXXX.com>
2018-04-17 16:33:24 +0800

    排序

--- src/main/java/Prog15.java
+++ src/main/java/Prog15.java
@@ -1,3 +1,4 @@
+
 /**
  15.排序
 -------
@@ -8,9 +9,61 @@
 
  
  */
+import java.util.Scanner;
+
 public class Prog15 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean[] array = new boolean[101];
+		int[] a = new int[100];
+
+		int x = 1;
+
+		for (int i = 1; i <= 100; i++) {
+			if (x == 0) {
+				i = 100;
+			} else {
+				System.out.println("第" + i + "个数");
+				x = scanner.nextInt();
+				array[x] = true;
+			}
+
+		}
+
+		for (int i = 1; i < 101; i++) {
+			if (array[i]) {
+				System.out.print(i);
+				System.out.print("<");
+			}
+		}
+
+		// Scanner scanner = new Scanner(System.in);
+		// System.out.println("第一个数：");
+		// int num1 = scanner.nextInt();
+		// System.out.println("第二个数：");
+		// int num2 = scanner.nextInt();
+		// System.out.println("第二个数：");
+		// int num3 = scanner.nextInt();
+		//
+		// if (num3 < num2 && num2 < num1) {
+		// System.out.println(num3 + "<" + num2 + "<" + num1);
+		// }
+		// if (num3 < num1 && num1 < num2) {
+		// System.out.println(num3 + "<" + num1 + "<" + num2);
+		// }
+		// if (num2 < num3 && num3 < num1) {
+		// System.out.println(num2 + "<" + num3 + "<" + num1);
+		// }
+		// if (num2 < num1 && num1 < num3) {
+		// System.out.println(num2 + "<" + num1 + "<" + num3);
+		// }
+		// if (num1 < num2 && num2 < num3) {
+		// System.out.println(num1 + "<" + num2 + "<" + num3);
+		// }
+		// if (num1 < num3 && num3 < num2) {
+		// System.out.println(num1 + "<" + num3 + "<" + num2);
+		// }
 
 	}
 
