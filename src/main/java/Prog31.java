8177540e63e187f391b87515757e15f5bc013532
yc <XXXXXX.com>
2018-04-18 17:05:40 +0800

    一个数组逆序输出

--- src/main/java/Prog31.java
+++ src/main/java/Prog31.java
@@ -1,15 +1,21 @@
 /**
- 31.将一个数组逆序输出。
------------------------
-
-程序分析：用第一个与最后一个交换。
-
- 
+ * 31.将一个数组逆序输出。 -----------------------
+ * 
+ * 程序分析：用第一个与最后一个交换。
+ * 
+ * 
  */
 public class Prog31 {
 
 	public static void main(String[] args) {
+		int a[] = new int[100];
+		for (int x = 0; x < a.length; x++) {
+			a[x] = x;
+		}
 
+		for (int i = a.length-1; i >= 0; i--) {
+			System.out.println(a[i]);
+		}
 	}
 
 }
