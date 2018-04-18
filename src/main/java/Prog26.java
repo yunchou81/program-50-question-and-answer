97e021da7913cd7bc19715f67a286cdca2540fa4
yc <XXXXXX.com>
2018-04-18 16:55:40 +0800

    求100之内的素数

--- src/main/java/Prog26.java
+++ src/main/java/Prog26.java
@@ -7,12 +7,21 @@
 判断第二个字母。
 
 程序分析：用情况语句比较好，如果第一个字母一样，则判断用情况语句或if语句判断第二个字母。
-
+星期一      Monday
+星期二      Tuesday
+星期三      Wednesday
+星期四      Thursday
+星期五      Friday
+星期六      Saturday
+星期天      Sunday
  */
+import java.util.Scanner;
 public class Prog26 {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("一个数给我：");
+		char num = scanner.nextChar();
 	}
 
 }
--- src/main/java/Prog27.java
+++ src/main/java/Prog27.java
@@ -1,12 +1,33 @@
 /**
- 27.求100之内的素数
-------------------
- 
+ * 27.求100之内的素数 ------------------
+ * 
  */
 public class Prog27 {
 
 	public static void main(String[] args) {
+		int[] a = new int[101];
 
+		int i = 2;
+		boolean flag = true;
+		while (flag) {
+			while (i < 101) {
+				if (a[i] == 0) {
+					for (int j = 2; (i * j) < 101; j++) {
+						a[i * j] = 1;
+					}
+					i++;
+				} else {
+					i++;
+				}
+			}
+			for (int j = 1; j < a.length; j++) {
+				if (j >= 1 && j <= 100 && a[j] == 0) {
+
+					System.out.println( + j + "是质素");
+				}
+			}
+			flag = false;
+		}
 	}
 
 }
