8d44ec8c8d10a30cf50be3e3215b14f713b74d73
yc <XXXXXX.com>
2018-04-18 16:37:08 +0800

    倒序打印

--- src/main/java/Prog24.java
+++ src/main/java/Prog24.java
@@ -1,3 +1,4 @@
+
 /**
  24.倒序打印
 -----------
@@ -6,10 +7,29 @@
 
  
  */
+import java.util.Scanner;
+
 public class Prog24 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("一个数给我：");
+		int num = scanner.nextInt();
+		int num1 = num;
+		int x = 1;
+		int h = 0;
 
+		while (num1 > 10) {
+			num = num1 % 10;
+			num1 = (num1 - num) / 10;
+			h = h * 10 + num;
+			x++;
+			
+		}
+		h = h * 10 + num1;
+
+		System.out.println("是" + x + "位数");
+		System.out.println(h);
 	}
 
 }
